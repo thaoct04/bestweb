011ca095bd8ce5ef371c8a45dae3b883561708bf
nguyen thuan thao <thaont.23itb@vku.udn.vn>
2024-05-22 09:55:01 +0700

    best project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./dist/**/*.{html,js}", "./index.html", "./css/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
